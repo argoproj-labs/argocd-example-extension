b26103d8596db548ebfe5bc3b56a2492246f91cc
Alex Collins <alex_collins@intuit.com>
2022-03-24 11:21:30 -0700

    fix: fetch data

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
+import {useEffect, useState} from 'react';
+
+
+const HelloButton = () => {
+    const [text, setText] = useState<string>();
+
+    useEffect(() => {
+        fetch("/api/extensions/hello")
+            .then(() => setText('Success'))
+            .catch(e => setText(e))
+    }, [])
+
+    return <>{text}</>
+}
 
 export const helloButton = {
     type: 'apptoolbar',
     factory: () => ({
         iconClassName: 'fa fa-info-circle',
-        title: <>Hello</>,
+        title: <HelloButton/>,
         action: () => {
             alert("hello!")
         }
