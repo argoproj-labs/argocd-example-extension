cc23e618bd1377d99c37a3793d8ae6752928212c
Alex Collins <alex_collins@intuit.com>
2022-03-25 15:31:01 -0700

    fix: ok

--- v2/ui/src/index.tsx
+++ v2/ui/src/index.tsx
@@ -5,11 +5,11 @@ import ExampleChart from "./exampleChart";
 
 type Metadata = { name: string };
 
-export const helloButton = {
-    type: 'appToolbar',
+export const appToolbarButton = {
+    type: 'appToolbarButton',
     factory: ({setState}: { setState: (value: any) => void }) => ({
         iconClassName: 'fa fa-chart-line',
-        title: <>Open chart</>,
+        title: <>Charts</>,
         action: () => setState({isShown: true})
     })
 }
@@ -24,27 +24,42 @@ const HelloPanel = ({application}: { application: any }) => {
             .catch(e => setMessage(e))
     }, [])
 
-    return <div><h1>{application.metadata.name} </h1>
+    return <div><h1>{application.metadata.name}</h1>
         <h2>API says "{message}"</h2>
         <ExampleChart width={1024} height={640}/>
     </div>
 }
-export const helloPanel = {
+export const appPanel = {
     type: 'appPanel',
-    factory: ({state, setState}: { state: any, setState: (value: any) => void }) => ({
+    factory: ({state, setState, application}: { state: any, setState: (value: any) => void, application: any }) => ({
         isShown: state.isShown,
-        onClose:  () => setState({isShown: false}),
-        component: HelloPanel
+        onClose: () => setState({isShown: false}),
+        component: <HelloPanel application={application}/>
     })
 }
 
 type Resource = { metadata: Metadata };
 
-const ResourcePanel = ({resource}: { tree: any, resource: Resource }) =>
+const ResourcePanel = ({tree, resource}: { tree: any, resource: Resource }) =>
     <div>Hello {resource.metadata.name}!</div>;
 
 export const resourcePanel = {
     type: 'resourcePanel',
-    factory: () => ({component: ResourcePanel})
+    factory: ({resource, tree}: { resource: any, tree: any }) => ({
+        component: <ResourcePanel resource={resource} tree={tree}/>
+    })
 }
 
+const OpenGraphPanelItem = ({onClick}: { onClick: () => void }) =>
+    <a onClick={onClick} style={{color: 'red', fontSize: "200%"}}>
+        <i className='fa fa-chart-line'/>
+        &nbsp;
+        Charts
+    </a>;
+
+export const statusPanelItem = {
+    type: 'appStatusPanelItem',
+    factory: ({state, setState}: { state: any, setState: (value: any) => void }) => ({
+        component: <OpenGraphPanelItem onClick={() => setState({isShown: true})}/>
+    })
+}
