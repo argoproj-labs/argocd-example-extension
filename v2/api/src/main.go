70ccc3aadcc8918c3aee3eabb835c51f78660b9b
Alex Collins <alex_collins@intuit.com>
2022-03-29 11:42:18 -0700

    feat: log claims-sub

--- v2/api/src/main.go
+++ v2/api/src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -11,6 +12,7 @@ type response struct {
 
 func main() {
 	err := http.ListenAndServe(":3983", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("claims-sub=%s\n", r.Header.Get("claims-sub"))
 		if r.Header.Get("Authorization") != "Bearer LetMeIn" {
 			w.WriteHeader(http.StatusForbidden)
 			return
